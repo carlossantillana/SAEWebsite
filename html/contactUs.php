d863002ff41aada0740708cd38aaf12262ec0bf8
Carlos Santillana <csant019@ucr.edu>
2018-01-20 14:53:49 -0800

    fixed contact us page, and fixed navbar

---
+++ html/contactUs.php
@@ -0,0 +1,25 @@
+
+<?php
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: FSAE';
+    // $to = 'test@fsae.com';
+    $to = 'hqw0p@slipry.net';
+    $subject = 'Hello from the Website';
+    $human = $_POST['human'];
+    $name = $fname + $lname;
+
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+    if ($_POST['submit'] && $human == '6') {
+        if (mail ($to, $subject, $body, $from)) {
+	    echo '<p>Your message has been sent!</p>';
+	} else {
+	    echo '<p>Something went wrong, go back and try again!</p>';
+	}
+} else if ($_POST['submit'] && $human != '6') {
+	echo '<p>You answered the anti-spam question incorrectly!</p>';
+    }
+?>
