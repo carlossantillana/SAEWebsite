674926fdf52777259547049fe24c9f7f98d0aeaf
Carlos Santillana <csant019@ucr.edu>
2018-01-25 22:20:35 -0800

    updated officers page

--- html/contactUs.php
+++ html/contactUs.php
@@ -5,7 +5,7 @@
     $email = $_POST['email'];
     $message = $_POST['message'];
     $from = 'From: FSAE';
-    // $to = 'test@fsae.com';
+    // $to = 'test@fsae.com';       
     $to = 'hqw0p@slipry.net';
     $subject = 'Hello from the Website';
     $human = $_POST['human'];
