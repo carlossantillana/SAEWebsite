4bbc79420769c4b310ae095276bdfc87c6d2437f
Carlos Santillana <csant019@ucr.edu>
2018-02-10 22:38:42 -0800

    updated team photo

---
+++ _site/html/contactUs.php
@@ -0,0 +1,24 @@
+
+<?php
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: FSAE';
+    $to = 'rlugt002@ucr.edu';
+    $subject = 'Hello from the Website';
+    $human = $_POST['human'];
+    $name = $fname + $lname;
+
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+    if ($_POST['submit'] && $human == '6') {
+        if (mail ($to, $subject, $body, $from)) {
+	    echo '<p>Your message has been sent!</p>';
+	} else {
+	    echo '<p>Something went wrong, go back and try again!</p>';
+	}
+} else if ($_POST['submit'] && $human != '6') {
+	echo '<p>You answered the anti-spam question incorrectly!</p>';
+    }
+?>
---
+++ _site/js/home.js
@@ -0,0 +1,4 @@
+var vid = document.getElementById("vide");
+function setPlaySpeed() {
+    vid.playbackRate = 1.6;
+}
